3e79a91f6e4f02179b4e741f145e8d8d8c08de35
monkaCat.jar | MeSSengeR.py <139318962+messengernew@users.noreply.github.com>
2024-06-01 23:31:34 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import LinkButton from '@/components/LinkButton';
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[75vh]">
-      <h1 className="text-[2rem] md:text-[3.5rem] font-extrabold after:content-['Fox🦊'] after:text-orange-600">
-        Hi, i`m Krypton
+      <h1 className="text-[2rem] md:text-[3.5rem] font-extrabold">
+        Hi, i`m Krypton<a class="text-orange-600">Fox🦊</a>
       </h1>
       <div className="flex flex-wrap justify-center text-center text-sm md:text-base space-x-2">
         <LinkButton href="/about">Обо мне</LinkButton>
