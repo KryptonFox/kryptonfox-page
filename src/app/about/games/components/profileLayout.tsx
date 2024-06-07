f6bf2c04127ef01fa5b34b6f049ffa7760a9c0a1
KryptonFox <kirieshka0785@gmail.com>
2024-06-07 17:05:12 +0300

    /about/games update

--- src/app/about/games/components/profileLayout.tsx
+++ src/app/about/games/components/profileLayout.tsx
@@ -4,19 +4,18 @@ import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid';
 
 export default function ProfileLayout() {
   return (
-    <div className="border border-zinc-700 rounded-xl text-center">
-      <div className="bg-zinc-800 rounded-t-xl py-3">Мой профиль</div>
-      <div className="h-[380px] w-[220px] bg-zinc-800/30">
+    <div className="border border-zinc-800 rounded-xl text-center">
+      <div className="bg-zinc-900 rounded-t-xl py-3">Мой персонаж
+      <a
+        className="hover:underline hover:text-zinc-300 transition flex items-center justify-center"
+        href="https://ru.namemc.com/profile/TheKryptonFox"
+        target='blank'
+      >
+        <span>TheKryptonFox</span>
+        <ArrowTopRightOnSquareIcon className="ml-1 w-5 h-5" />
+      </a></div>
+      <div className="h-[380px] w-[300px] bg-zinc-800/30">
         <Skin />
-      </div>
-      <div className="bg-zinc-800 rounded-b-xl py-3 w-full">
-        <a
-          className="hover:underline hover:text-zinc-300 transition flex items-center justify-center"
-          href="https://ru.namemc.com/profile/TheKryptonFox"
-        >
-          <span>TheKryptonFox</span>
-          <ArrowTopRightOnSquareIcon className="ml-1 w-5 h-5" />
-        </a>
       </div>
     </div>
   );
--- src/app/about/games/page.tsx
+++ src/app/about/games/page.tsx
@@ -3,18 +3,16 @@ import ProfileLayout from '@/app/about/games/components/profileLayout';
 export default function page() {
   return (
     <>
-      <h1 className="text-4xl mb-3 font-extrabold">Игры в которые я играю</h1>
-      <h2 className="text-3xl font-bold mb-4">Майнкраааафффт!</h2>
-      <div className="flex flex-wrap justify-center md:justify-between">
-        <p className="flex-1 min-w-[220px] pr-8 pb-4 text-lg text-justify">
-          Классная игра. Одна из первых игр в которые я начал играть. Играю в
-          неё до сих пор. Играю в основном на лицензионных серверах с
-          мини-играми, а также на приватных
+      <h1 className="text-4xl mb-3 font-extrabold text-center">Игры</h1>
+      <h2 className="text-3xl font-bold mb-4 text-center">Майнкрафт!</h2>
+      <div className="flex flex-wrap justify-center md:justify-between space-x-4">
+        <p className="flex-1 min-w-[220px] pb-4 text-lg text-justify">
+          Одна из первых моих игр и играю в неё до сих пор. Очень уж нравится мне тут проводить время с друзьями. Играю обычно на СП, а также других приватных серверах
         </p>
         <ProfileLayout />
       </div>
-      <p className="py-2 text-xl">
-        Иногда играю во что-то другое по настроению :)
+      <p className="py-2 text-justify">
+        Иногда играю во что-то другое по настроению, но это очень редко т.к. я нахожу себе занятие поинтереснее :)
       </p>
     </>
   );
