77b761770d2d8d89e4cbeb2d45afddd6979d8ff0
KryptonFox <kirieshka0785@gmail.com>
2024-06-07 17:17:13 +0300

    update bg colors opacity

--- src/components/SidebarTab.tsx
+++ src/components/SidebarTab.tsx
@@ -13,8 +13,8 @@ export default function SidebarTab({ name, href }: SidebartabProps) {
     <Link
       className={
         href === pathname
-          ? 'hover:bg-zinc-800 px-4 py-1.5 rounded-lg my-1 transition duration-300 text-lg font-extrabold text-yellow-400 bg-yellow-600/10'
-          : 'hover:bg-zinc-800 px-4 py-1.5 rounded-lg my-1 transition duration-300 text-lg font-semibold'
+          ? 'hover:bg-zinc-800/50 px-4 py-1.5 rounded-lg my-1 transition duration-300 text-lg font-extrabold text-yellow-400 bg-yellow-600/10'
+          : 'hover:bg-zinc-800/50 px-4 py-1.5 rounded-lg my-1 transition duration-300 text-lg font-semibold'
       }
       href={href}
     >
